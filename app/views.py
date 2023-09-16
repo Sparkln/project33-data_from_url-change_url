b409733f52de62d23e320b6a99905c6d94fd02d9
Sparkln/githuba1 <suryakanta67@gmail.com>
2023-09-16 10:50:20 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def data_from_url(request,Name):
+    return HttpResponse(f'Hiii {Name} How are you')
